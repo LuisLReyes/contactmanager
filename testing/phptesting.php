e8aef015b3cf38d0321b48bd75f6c463eb442785
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-05-28 21:33:15 -0400

    Comprehensive DB dump

--- testing/phptesting.php
+++ testing/phptesting.php
@@ -22,6 +22,26 @@
     else {
         echo "0 results";
     }
+
+    $sql = "SELECT first_name, last_name, id, users_id, phone_number, email, address  FROM users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " users_id: " . $row["users_id"]. " first_name: " . $row["first_name"]. " last_name: " . $row["last_name"]." phone_number: ".$row["phone_number"]. " email: ".$row["email"]." address: ".$row["email"]. "<br>";
+        }
+    } 
+    else {
+        echo "0 results";
+    }
+
+
+
+
+
+
+
 $conn->close();
 
 ?>
