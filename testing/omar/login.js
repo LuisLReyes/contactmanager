c9c4597ebff6cbcb80dbb67fcb42a94433d609f4
ThisIsNice <38233149+ThisIsNice@users.noreply.github.com>
2019-05-30 16:55:43 -0400

    Update login.js

--- testing/omar/login.js
+++ testing/omar/login.js
@@ -1,35 +1,12 @@
+$(function() {
+    console.log( "ready!" );
+});
 function doLogin()
 {
 	var login = document.getElementById("inputUserId").value;
 	var password = document.getElementById("inputPassword").value;
 	document.getElementById("loginResult").innerHTML = "";
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = 'C:/Users/casti/OneDrive/Desktop/Login/index.html?' + '/Login.aspx';
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.send(jsonPayload);
-		var jsonObject = JSON.parse( xhr.responseText );
-		userId = jsonObject.id;
-		if( userId < 1 )
-		{
-			document.getElementById("loginResult").innerHTML = "Username/Password combination incorrect";
-			return;
-		}
-		var firstName = jsonObject.firstName;
-		var lastName = jsonObject.lastName;
-		document.getElementById("userName").innerHTML = firstName + " " + lastName;
-	 	document.getElementById("loginName").value = document.getElementById("loginPassword").value = "";
-		hideOrShow( "loggedInDiv", true);
-		hideOrShow( "accessUIDiv", true);
-		hideOrShow( "loginDiv", false);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+
 }
 
 function doCreate()
