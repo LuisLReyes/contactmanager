749f18fb8524c816fbe69b037311785d7b292d07
ThisIsNice <38233149+ThisIsNice@users.noreply.github.com>
2019-06-08 18:02:14 -0400

    switching to using login helpers

--- javascript/login.js
+++ javascript/login.js
@@ -15,77 +15,27 @@ $(function() {
 
 function doLogin()
 {
-
-	// create user field also populates these ids so they can always be used to start
-	// the function
-	var userid = document.getElementById("inputUserId").value;
-	console.log(userid);
-	var password = document.getElementById("inputPassword").value;
-	console.log(password);
+   var userid = document.getElementById("inputUserId").value;
+   var password = document.getElementById("inputPassword").value;
 
 	// hash the password using the imported git library
 	var MD5 = new Hashes.MD5;
 	password = MD5.hex(password);
 
-	var jsonPayload = {user_name : userid, password : password};
-	jsonPayload = JSON.stringify(jsonPayload);
-
-	$.ajax({
-	    url: "https://contactmanager4331.herokuapp.com/api-files/api/user/login.php",
-	    type : "POST",
-	    contentType : 'application/json',
-	    data : jsonPayload,
-	    success : function(result){
-	        console.log("User Logged in");
-	        console.log(result);
-
-	        //Change page if you want
-	        //location = 'contacts.html';
-	    },
-	    error: function(xhr, resp, text){
-	        // on error, log it
-	        console.log("User not logged in");
-	        console.log(xhr);
-	        console.log(resp);
-	        console.log(test);
-					document.getElementById("loginResult").innerHTML = "incorrect username/password combination";
-	    }
-	});
-
+   // pass inputs to login helper
+   login(userid, password);
 }
 
 function doCreate()
 {
-	var uid = document.getElementById("inputUserId").value = document.getElementById("signUpId").value;
-	var password = document.getElementById("inputPassword").value = document.getElementById("signUpPassword").value;
+	var uid = document.getElementById("signUpId").value;
+	var password = document.getElementById("signUpPassword").value;
+   var fName = document.getElementById("signUpName").value;
 
 	// hash the password using the imported git library
 	var MD5 = new Hashes.MD5;
 	password = MD5.hex(password);
-	var fname = document.getElementById("signUpName").value;
 
-	// create object to hold all needed values to convert to JSON for creating user
-	var jsonPayload = {first_name : fname, user_name : uid, password : password};
-	var jsonPayload = JSON.stringify(jsonPayload);
-
-	// asynch call to create a new user field in the database
-	$.ajax
-	({
-		url:"https://contactmanager4331.herokuapp.com/api-files/api/user/createuser.php",
-		type: "POST",
-		contentType: 'application/json',
-		data: jsonPayload,
-		success: function(result)
-		{
-			console.log("User creation success");
-			console.log(jsonPayload);
-			console.log(result);
-			doLogin();
-		},
-		error: function(xhr, resp, text)
-		{
-			console.log("User creation fail");
-			// TODO display an error message to user
-		}
-	})
+   // pass the input to a helper functions that creates an logs in the userid
+   signup(uid, password, fName);
 }
---
+++ javascript/loginhelper.js
@@ -0,0 +1,57 @@
+
+//Call to create a new user
+function signup(signupUsername, signupPassword, signupFirstName,){
+    //Prepare data
+    var jsonPayload = JSON.stringify({first_name: signupFirstName, user_name : signupUsername, password : signupPassword});
+    //Ajax call
+    $.ajax({
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/user/createuser.php",
+        type : "POST",
+        contentType : 'application/json',
+        data : jsonPayload,
+        success : function(result){
+            console.log("User Created");
+            console.log(result);
+    
+            //Login after creating user
+            return login(signupUsername, signupPassword);
+        },
+        error: function(xhr, resp, text){
+            // on error, log it
+            console.log("User not logged in");
+            console.log(xhr);
+            console.log(resp);
+            consold.log(text);
+            //Return a failure
+            return xhr;
+        }
+    });
+}
+
+function login(loginUsername,loginPassword){
+
+    var jsonPayload = JSON.stringify({user_name : loginUsername, password : loginPassword});
+
+    $.ajax({
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/user/login.php",
+        type : "POST",
+        contentType : 'application/json',
+        data : jsonPayload,
+        success : function(result){
+            console.log("User Logged in");
+            console.log(result);
+    
+            //Change page if you want
+            location = 'contacts.html';
+        },
+        error: function(xhr, resp, text){
+            // on error, log it
+            console.log("User not logged in");
+            console.log(xhr);
+            console.log(resp);
+            console.log(text);
+            //return a failure
+            return xhr;
+        }
+    });
+}
