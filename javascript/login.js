97b2fee4736e5ac5106c1922770b05d1a78a61fb
ThisIsNice <38233149+ThisIsNice@users.noreply.github.com>
2019-06-08 19:17:05 -0400

    Update login.js
    currently removed form validation will do something about it but for creating a user

--- javascript/login.js
+++ javascript/login.js
@@ -1,32 +1,3 @@
-$(function() {
-    $("form[name = 'login']").validate({ // initialize the plugin
-        rules: {
-            inputUserId: {
-                required: true,
-            },
-            inputPassword: {
-                required: true,
-                minlength: 10
-            }
-
-        },
-       // Specify validation error messages
-    messages: {
-      inputPassword: {
-          required: "Please provide a password",
-          minlength: "Your password must be at least 10 characters long"
-      },
-      inputUserId: "Please enter a username"
-    },
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
-    submitHandler: function(form) {
-      form.submit();
-    }
-    });
-
-});
-
 function doLogin()
 {
    var userid = document.getElementById("inputUserId").value;
