36055ed89eb225763b9493cf207af12ed55058e7
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-06-08 21:54:18 -0400

    Update contacthelper.js

--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -1,20 +1,73 @@
 function getContacts(userID){
-    //Prepare data
     //Ajax call
     $.ajax({
-        url: "https://contactmanager4331.herokuapp.com/api-files/api/user/createuser.php?users_id=" + userID,
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/contact/getcontacts.php?users_id=" + userID,
         type : "GET",
         contentType : 'application/json',
         success : function(result){
             console.log("Contacts Retrieved:");
             console.log(result);
             
-            //Login after creating user
+            //Return contacts retrieved
             return result;
         },
         error: function(xhr, resp, text){
             // on error, log it
             console.log("Contacts unable to be loaded");
+            console.log(xhr);
+            console.log(resp);
+            consold.log(text);
+            //Return a failure
+            return xhr;
+        }
+    });
+}
+
+function addContact(contactToAdd){
+    //Ajax call
+    $.ajax({
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/contact/addcontact.php",
+        type : "POST",
+        data: contactToAdd,
+        contentType : 'application/json',
+        success : function(result){
+            console.log("Contacts Added");
+            console.log(result);
+            
+            //Return message
+            return result;
+        },
+        error: function(xhr, resp, text){
+            // on error, log it
+            console.log("Contacts was not added");
+            console.log(xhr);
+            console.log(resp);
+            consold.log(text);
+            //Return a failure
+            return xhr;
+        }
+    });
+}
+
+function deleteContact(contactID){
+    //Prepare data
+    var jsonPayload = JSON.stringify({id: contactID});
+    //Ajax call
+    $.ajax({
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/contact/deletecontact.php",
+        type : "DELETE",
+        data : jsonPayload,
+        contentType : 'application/json',
+        success : function(result){
+            console.log("Contact Deleted");
+            console.log(result);
+            
+            //Return result
+            return result;
+        },
+        error: function(xhr, resp, text){
+            // on error, log it
+            console.log("Contacts unable to be deleted");
             console.log(xhr);
             console.log(resp);
             consold.log(text);
