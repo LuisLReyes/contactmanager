016734d2c2511dd1f4e5c3bde79a284638dec054
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-06-09 19:47:32 -0400

    Logout function

--- api-files/api/system/logout.php
+++ api-files/api/system/logout.php
@@ -1,5 +1,12 @@
 <?php
     session_start();
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json;charset=UTF-8');
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
     session_unset(); 
     session_destroy();
+    return;
 ?>
--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -110,3 +110,26 @@ function updateContact(contactToEdit){
         }
     });
 }
+
+function logout(){
+    //Call the API to wipe the session
+    $.ajax({
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/system/logout.php",
+        type : "POST",
+        contentType : 'application/json',
+        success : function(result){
+            console.log("Logged out");            
+            //Logout
+            location = 'index.html';
+        },
+        error: function(xhr, resp, text){
+            // on error, log it
+            console.log("Failed to logout");
+            console.log(xhr);
+            console.log(resp);
+            consold.log(text);
+            //Return a failure
+            return xhr;
+        }
+    });
+}
