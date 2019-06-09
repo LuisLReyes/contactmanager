5a123e2281a0597feb4f42fa3eef3a1596dda090
AlexRavioli <alexandooop@gmail.com>
2019-06-09 01:55:14 -0400

    Create contact.js
    Slowly using my monkey brain to figure this out

---
+++ javascript/contact.js
@@ -0,0 +1,31 @@
+function fillContacts(userid){
+  
+}
+
+    $(".add-new").click(function(){
+      $(this).attr("disabled", "disabled");
+      var index = $("table tbody tr:last-child").index();
+          var row = '<tr>' +
+              '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
+              '<td><input type="text" class="form-control" name="Last Name" id="lname"></td>' +
+              '<td><input type="text" class="form-control" name="Email" id="email"></td>' +
+              '<td><input type="text" class="form-control" name="Phone Number" id="phone"></td>' +
+              '<td><input type="text" class="form-control" name="Address" id="address"></td>' +
+        '<td>' + action + '</td>' +
+          '</tr>';
+        $("table").append(row);		
+      $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+          $('[data-toggle="tooltip"]').tooltip();
+    });
+
+	$(document).on("click", ".add", function(){
+		var empty = false;
+		var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function(){
+			if(!$(this).val()){
+				$(this).addClass("error");
+				empty = true;
+			} else{
+                $(this).removeClass("error");
+            }
+		});
