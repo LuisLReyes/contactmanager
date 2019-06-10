383eabf1813c91e80c1440a74c11d07d148119c8
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-06-10 16:03:22 -0400

    Update contacthelper.js

--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -22,7 +22,7 @@ function displayContacts(userID){
                     '<td id="email'+contact.id+'">'+JSON.stringify(contact.email).replace(/['"]+/g, '')+'</td>' +
                     '<td id="phone_number'+contact.id+'">'+JSON.stringify(contact.phone_number).replace(/['"]+/g, '') +'</td>' +
                     '<td id="address'+contact.id+'">'+JSON.stringify(contact.address).replace(/['"]+/g, '')+'</td>' +
-                    '<td id="buttons'+contact.id+'" style="text-align:center"> <span class="oi oi-cog" style="width:50%"></span>   <span class="oi oi-x" style="width:50%">></span></td>' +
+                    '<td id="buttons'+contact.id+'" style="text-align:center"> <span class="oi oi-cog" style="width:40%"></span><span class="oi oi-x" style="width:40%"></span></td>' +
                     '</tr>';
                 $("table").prepend(row);
             });
