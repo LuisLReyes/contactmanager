8a86c2d6c9d161ab8cce02639d6df2039331fc25
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-06-09 22:30:12 -0400

    Update contacthelper.js

--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -17,8 +17,13 @@ function displayContacts(userID){
             
             result.data.forEach(contact =>  {
                 console.log(contact);
-                var td1 = '<td id="first_name'+contact.id+'">'+JSON.stringify(contact.first_name)+'</td>';
-                $("table").prepend(td1);
+                var row = '<td id="first_name'+contact.id+'">'+JSON.stringify(contact.first_name)+'</td>' + 
+                    '<td id="last_name'+contact.id+'">'+JSON.stringify(contact.last_name)+'</td>' + 
+                    '<td id="email'+contact.id+'">'+JSON.stringify(contact.email)+'</td>' +
+                    '<td id="phone_number'+contact.id+'">'+JSON.stringify(contact.phone_number) +'</td>' +
+                    '<td id="address'+contact.id+'">'+JSON.stringify(contact.address)+'</td>' + 
+                    '<td id="buttons'+contact.id+'"> :^) </td>';
+                $("table").prepend(row);
             });
 
             //Return contacts retrieved
