1bd4c69d2e7a427878a94a8f6bef21fdb7fbf6ae
ThisIsNice <38233149+ThisIsNice@users.noreply.github.com>
2019-06-10 17:50:37 -0400

    Update contacthelper.js

--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -186,7 +186,7 @@ function logout(){
 
 function editContact(contactId){
 
-   var row = '<tr id = '+contactId+'> '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
+   var row = '<tr id = '+contactId+'>' + '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
     '<td><input type="text" class="form-control" name="Last Name" id="lname"></td>' +
     '<td><input type="text" class="form-control" name="Email" id="email"></td>' +
     '<td><input type="text" class="form-control" name="Phone Number" id="phone"></td>' +
