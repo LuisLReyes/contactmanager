43c0e84e840c5a916c8406cea28b5de0b8ccda14
ThisIsNice <38233149+ThisIsNice@users.noreply.github.com>
2019-06-10 17:47:04 -0400

    Edit

--- javascript/contact.js
+++ javascript/contact.js
@@ -29,27 +29,3 @@ function saveContact(){
 	contactObj = JSON.stringify(contactObj);
   addContact(contactObj);
 }
-
-function editContact(contactId){
-
-   var row = '<tr id = '+contactId+'> '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
-    '<td><input type="text" class="form-control" name="Last Name" id="lname"></td>' +
-    '<td><input type="text" class="form-control" name="Email" id="email"></td>' +
-    '<td><input type="text" class="form-control" name="Phone Number" id="phone"></td>' +
-    '<td><input type="text" class="form-control" name="Address" id="address"></td>' +
-       '<td><span class="oi oi-circle-check" onclick="editHelper('+contactId+')"></span></td>' +
-       '</tr>';
-   $("table").append(row);
-
-}
-
-function editHelper(contactId){
-   var fname = document.getElementById('fname').value;
-   var lname = document.getElementById('lname').value;
-   var email = document.getElementById('email').value;
-   var phone = document.getElementById('phone').value;
- 	var address = document.getElementById('address').value;
-   var updateObj = {users_id : 1, first_name : fname, last_name : lname, phone_number : phone, address : address, email : email, id : contactId};
-   updateObj = JSON.stringify(updateObj);
-   updateContact(updateObj);
-}
