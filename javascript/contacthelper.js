88d910773739c9b32f77bf3c18df9ce23f90e72f
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-06-09 22:01:16 -0400

    Update contacthelper.js

--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -1,9 +1,51 @@
 var contactData;
 //This runs when the page is loaded
 $(function(){
-    contactData = getContacts(1); 
+    contactData = getContacts(userID); 
+    displayContacts(1);
 });
 
+
+function displayContacts(userID){
+    $.ajax({
+        url: "https://contactmanager4331.herokuapp.com/api-files/api/contact/getcontacts.php?users_id=" + userID,
+        type : "GET",
+        contentType : 'application/json',
+        success : function(result){
+            console.log("Contacts Retrieved:");
+            console.log(result);
+            
+            result.data.forEach(contact =>  {
+                console.log(contact);
+
+
+            });
+
+            //Return contacts retrieved
+            return result;
+        },
+        error: function(xhr, resp, text){
+            // on error, log it
+            console.log("Contacts unable to be loaded");
+            console.log(xhr);
+            console.log(resp);
+            consold.log(text);
+            //Return a failure
+            return xhr;
+        }
+    });
+
+
+    var row = '<tr>' +
+	      '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
+	      '<td><input type="text" class="form-control" name="Last Name" id="lname"></td>' +
+	      '<td><input type="text" class="form-control" name="Email" id="email"></td>' +
+	      '<td><input type="text" class="form-control" name="Phone Number" id="phone"></td>' +
+	      '<td><input type="text" class="form-control" name="Address" id="address"></td>' +
+	  '<td><button class="btn btn-primary my-2 my-sm-0 add-new" onclick="" type="submit">Save</button></td>' +
+	  '</tr>';
+	//$("table").prepend(row);		
+}
 
 
 function getContacts(userID){
