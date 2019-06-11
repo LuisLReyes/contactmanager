8edbafb4a8b637f89f1d4931a712e8e0c16e3821
LuisLReyes <43256028+LuisLReyes@users.noreply.github.com>
2019-06-10 21:05:38 -0400

    Merging js files

--- contact.php
+++ contact.php
@@ -88,7 +88,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <!--Place your custom JavaScript import below this line-->
-	<script src="javascript/contact.js"></script>
+	<!--<script src="javascript/contact.js"></script>-->
 	<script src="javascript/contacthelper.js"></script>
 			<script>
 				loggedInId =<?php echo $_SESSION["id"];?>;
--- javascript/contacthelper.js
+++ javascript/contacthelper.js
@@ -216,6 +216,7 @@ function editHelper(contactId){
    updateContact(updateObj);
 }
 
+
 function contactSearch(){
     var input, filter, table, tr, td, i, value, inRow;
     input = document.getElementById("searchbar");
@@ -271,3 +272,34 @@ function contactSearch(){
         
     }
 }
+
+
+
+
+function addRow(){
+
+    // Fix this
+          var index = $("table tbody tr:last-child").index();
+          var row = '<tr>' +
+              '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
+              '<td><input type="text" class="form-control" name="Last Name" id="lname"></td>' +
+              '<td><input type="text" class="form-control" name="Email" id="email"></td>' +
+              '<td><input type="text" class="form-control" name="Phone Number" id="phone"></td>' +
+              '<td><input type="text" class="form-control" name="Address" id="address"></td>' +
+                    '<td><span class="oi oi-circle-check" onclick="saveContact()"></span></td>' +
+          '</tr>';
+        $("table").prepend(row);
+    
+    }
+    
+    function saveContact(){
+      var fname = document.getElementById('fname').value;
+      var lname = document.getElementById('lname').value;
+      var email = document.getElementById('email').value;
+      var phone = document.getElementById('phone').value;
+        var address = document.getElementById('address').value;
+        var contactObj = {users_id : loggedInId, first_name : fname, last_name : lname, phone_number : phone, address : address, email : email};
+        console.log(contactObj);
+        contactObj = JSON.stringify(contactObj);
+      addContact(contactObj);
+    }
