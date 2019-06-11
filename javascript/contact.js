7d4b9217bbc6d9843f1838d26dd37f0f087b4adc
ThisIsNice <38233149+ThisIsNice@users.noreply.github.com>
2019-06-10 23:54:34 -0400

    Update contact.js

--- javascript/contact.js
+++ javascript/contact.js
@@ -6,16 +6,20 @@ function addRow(){
 
 // Fix this
       var index = $("table tbody tr:last-child").index();
-	  var row = '<tr>' +
+	  var row = '<tr id="newRow">' +
 	      '<td><input type="text" class="form-control" name="First Name" id="fname"></td>' +
 	      '<td><input type="text" class="form-control" name="Last Name" id="lname"></td>' +
 	      '<td><input type="text" class="form-control" name="Email" id="email"></td>' +
 	      '<td><input type="text" class="form-control" name="Phone Number" id="phone"></td>' +
 	      '<td><input type="text" class="form-control" name="Address" id="address"></td>' +
-				'<td><span class="oi oi-circle-check" onclick="saveContact()"></span></td>' +
+				'<td style="text-align:center"><span class="oi oi-circle-check" style="width:40%" onclick="saveContact()"></span><span class="oi oi-x" style="width:40%" onclick="removeRow()"></span></td>' +
 	  '</tr>';
 	$("table").prepend(row);
 
+}
+
+function removeRow(){
+  $('#newRow').remove();
 }
 
 function saveContact(){
